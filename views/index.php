df9351f15a892dd917312160f7a2d1c730b2a118
minhphuong2001 <minhphuongdo2001@gmail.com>
2022-06-11 17:31:03 +0700

    update product card ui

--- views/index.php
+++ views/index.php
@@ -160,8 +160,9 @@
                     foreach ($products as $key => $value) {
                 ?>
                     
-                        <div class="col-3 col-md-6 col-sm-12">
+                        <div class="col-3 col-md-6 col-sm-12 cards">
                             <div class="product-card">
+                                <div class="product-discount"><?= $value->discount?>%</div>
                                 <a href="product-detail?pro_id=<?=$value->id?>">
                                     <div class="product-card-img">
                                         <img src="images/<?=$value->get_images($con)[0]->link ?>" alt="">
@@ -174,16 +175,13 @@
                                         <button class="btn-flat btn-hover btn-cart-add">
                                             <i class='bx bxs-cart-add'></i>
                                         </button>
-                                        <button class="btn-flat btn-hover btn-cart-add">
-                                            <i class='bx bxs-heart'></i>
-                                        </button>
                                     </div>
                                     <a href="product-detail?pro_id=<?=$value->id?>" class="product-card-name">
                                         <?= $value->title?>
                                     </a>
                                     <div class="product-card-price">
-                                        <span><del><?= $value->price?></del></span>
-                                        <span class="curr-price"><?= $value->priceDiscount()?></span>
+                                        <span><del>$<?= $value->price?></del></span>
+                                        <span class="curr-price">$<?= $value->priceDiscount()?></span>
                                     </div>
                                 </div>
                             </div>
