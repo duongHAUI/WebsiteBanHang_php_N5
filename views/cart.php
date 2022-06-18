14e1533584c9eb9b74201836ede4b07ab74b5821
Nguyễn Đông <nguyenbadong01@gmail.com>
2022-06-18 20:43:54 +0700

    merge branch main into 9-cart-page

--- views/cart.php
+++ views/cart.php
@@ -35,7 +35,8 @@ for ($i = 0; $i < count($carts); $i++) {
 </head>
 
 <body>
-  <<<<<<< HEAD <div class="container">
+
+  <div class="container">
     <div class="header">
       <a href="products">
         <i class='bx bx-chevron-left'></i> SẢN PHẨM
@@ -44,8 +45,10 @@ for ($i = 0; $i < count($carts); $i++) {
     </div>
     <h1>GIỎ HÀNG</h1>
     <div class="list">
+
       <?php
       if (count($carts) == 0) {
+<<<<<<< HEAD
         echo '<p class="no-product">Không có sản phẩm nào trong giỏ hàng.</p>';
       }else{
             $total = 0;
@@ -77,15 +80,48 @@ for ($i = 0; $i < count($carts); $i++) {
             ?>
             <div class="checkout">
               <a href="checkout" class="btn-checkout"><?= currency_format($total) ?> <span></span>Thanh toán</a>
+=======
+        echo '<p class="no-product">No products in your cart.</p>';
+      } else {
+        $total = 0;
+      ?>
+        <div class="item-header">
+          <p style="width: calc(100px + 35%);">Sản phẩm</p>
+          <p style="flex: 1; text-align: center;">Đơn giá</p>
+          <p style="flex: 1; text-align: center;">Số lượng</p>
+          <p style="flex: 1; text-align: center;">Tổng tiền</p>
+          <p>Thao tác</p>
+        </div>
+        <?php
+        foreach ($carts as $index => $cart) {
+          $last_item = $index == count($carts) - 1 ? "last-item" : "";
+          $product = $cart->product;
+
+          $thumb = $product->get_images($con)[0]->link;
+          $name = $product->title;
+          $quantity = $cart->qty;
+          $price = $product->price;
+          $price_discount = $product->get_price_discount();
+          $item_total = $product->price * $quantity * (100 - $product->discount) / 100;
+          $total += $item_total;
+        ?>
+          <div class="item <?= $last_item ?>">
+            <div class="item_img">
+              <img src="images/<?= $thumb ?>" alt="">
+>>>>>>> 0858c81 (update ui cart)
             </div>
             <p class="item_name"><?= $name ?></p>
+            <div class="item-price">
+              <span class="price-not-discount"><?= $price ?></span>
+              <span class="price-discount"><?= $price_discount ?></span>
+            </div>
             <div class="item_qty">
               <a href="update-cart?cart_id=<?= $cart->id ?>&cart_qty=<?= $quantity - 1 ?>"><i class='bx bx-minus'></i></a>
               <input type="text" oninput="handleInput(event)" onchange="handleChange(event, <?= $cart->id ?>)" value="<?= $quantity ?>" />
               <a href="update-cart?cart_id=<?= $cart->id ?>&cart_qty=<?= $quantity + 1 ?>"><i class='bx bx-plus'></i></a>
             </div>
-            <div class="item_price">$<?= $price ?></div>
-            <a href="update-cart?cart_id=<?= $cart->id ?>&cart_qty=0"><i class='bx bx-x bx-rotate-90'></i></a>
+            <div class="item-total">$<?= $item_total ?></div>
+            <a href="update-cart?cart_id=<?= $cart->id ?>&cart_qty=0"><i class='bx bx-x bx-rotate-90'></i> Xóa</a>
           </div>
         <?php
         }
@@ -94,49 +130,23 @@ for ($i = 0; $i < count($carts); $i++) {
           <a href="checkout" class="btn-checkout">$<?= number_format($total, 2) ?> <span></span>Checkout</a>
         </div>
     </div>
-    </div>
-    <script>
-      function handleInput(event) {
-        let value = event.target.value;
-        console.log(event.target.value);
-        event.target.value = value.replace(/[^0-9]/g, '');
-      }
+  </div>
+  <script>
+    function handleInput(event) {
+      let value = event.target.value;
+      console.log(event.target.value);
+      event.target.value = value.replace(/[^0-9]/g, '');
+    }
 
-      function handleChange(event, cart_id) {
-        let quantity = event.target.value;
-        window.open(`update-cart?cart_id=${cart_id}&cart_qty=${quantity}`, "_self")
-      }
-    </script>
-  <?php
+    function handleChange(event, cart_id) {
+      let quantity = event.target.value;
+      window.open(`update-cart?cart_id=${cart_id}&cart_qty=${quantity}`, "_self")
+    }
+  </script>
+<?php
       }
       include_once("footer.php");
-  ?>
-  =======
-  <div class="container">
-    <div class="header">
-      <a href="shop">
-        <i class='bx bx-chevron-left'></i> SHOP
-      </a>
-      <a href="javascript:history.go(-1)"><i class='bx bx-x bx-rotate-90'></i></a>
-    </div>
-    <h1>Giỏ hàng</h1>
-    <div class="list">
-      <div class="item">
-        <div class="item_img">
-          <img src="images/JBL_TUNE220TWS_ProductImage_Pink_Back.png" alt="">
-        </div>
-        <p class="item_name">Tai nghe Bluetooth AirPods 2 Wireless</p>
-        <div class="item_qty">
-          <a href="update-cart?cart_id=11&cart_qty=2"><i class='bx bx-minus'></i></a>
-          <p>3</p>
-          <a href="update-cart?cart_id=11&cart_qty=4"><i class='bx bx-plus'></i></a>
-        </div>
-        <div class="item_price">5000</div>
-        <a href="delete-cart?cart_id=11"><i class='bx bx-x bx-rotate-90'></i></a>
-      </div>
-    </div>
-  </div>
-  >>>>>>> cc2ed63 (Add cart page)
+?>
 </body>
 
 </html>
