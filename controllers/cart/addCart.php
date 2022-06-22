72e2eb69ea48849e6860d779627a7b6f494e60bf
duongHAUI <duongvipro2k1@gmail.com>
2022-06-22 22:30:22 +0700

    fix-add-to-cart

--- controllers/cart/addCart.php
+++ controllers/cart/addCart.php
@@ -4,18 +4,24 @@
     include "../../models/index.php";
     session_start();
     if(!isset($_SESSION['c_user'])){
-        header("Location: ../../login");
+        header("HTTP/1.1 401 Unauthorized");
+        exit;
     }else{
         $cus_id = $_SESSION['c_user']['id'];
         $pro_id = $_GET['pro_id'];
         $quantity = (int)$_GET['quantity'];
         $cart = Cart::find_one($con,array("where"=>"cus_id='$cus_id' and pro_id = '$pro_id'"));
-        
         if( $cart !=null){
             $update_qty = $quantity + $cart->qty;
             $product = product::find_one($con,array("where"=>"product_id = '$pro_id'"));
-            $qty = ($update_qty <= (int)$product->quantity) ? $update_qty : $product->quantity;
-            $update = Cart::update_by_pk($con,$cart->id,array("cart_qty"=>$qty));
+            if($product->quantity < $quantity){
+                header("HTTP/1.1 403 Unauthorized");
+                exit;
+            }else{
+                $qty = ($update_qty <= (int)$product->quantity) ? $update_qty : $product->quantity;
+                $update = Cart::update_by_pk($con,$cart->id,array("cart_qty"=>$qty));
+            }
+            
         }else{
             $rs = Cart::create($con,array('cart_qty'=>$quantity,'cus_id'=>$cus_id,'pro_id'=>$pro_id));
         }
@@ -27,7 +33,7 @@
             foreach ($carts as $key => $value) {
             $count += 1;
         }
-        echo $count;
+        echo $product->quantity;
     }
 
 ?>
--- js/addCartHeader.js
+++ js/addCartHeader.js
@@ -1,18 +1,30 @@
+
 let quantity = document.querySelector("#quantity");
 let addCart = document.querySelector("#add-to-cart-prodetail");
 function addToCart(id,quantity){
-    if(quantity == 0){
-        alert("Sản phẩm đã hết hàng!");
-    }else{
-        alert("Sản phẩm đã được thêm vào giỏ hàng!");
         var xmlhttp=new XMLHttpRequest();
+        
         xmlhttp.onreadystatechange=function() {
-            if (this.readyState==4 && this.status==200) {
+            
+            if(this.status == 403){
+                alert("Sản phẩm đã hết hàng!");
+            }
+            else if (this.readyState==4 && this.status==200) {
+                alert("Sản phẩm đã thêm vào giỏ hàng!");
                 document.getElementById("countCart").innerText=this.responseText;
             }
+            else if(this.status == 401){
+                console.log("401");
+                window.location="http://localhost/WebsiteBanHang_php_N5/login";
+            }
         }
+        
         xmlhttp.open("GET","/WebsiteBanHang_php_N5/controllers/cart/addCart.php?noNavigate=true&quantity=1&pro_id="+id,true);
         xmlhttp.send();
-    }
-    
+        
 }
+
+
+
+
+
