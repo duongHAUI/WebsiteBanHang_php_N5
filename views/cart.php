25c73f71a736c1581ef5431127b2538990be5800
Nguyễn Đông <nguyenbadong01@gmail.com>
2022-06-23 00:09:38 +0700

    update logic and change quantity product in cart

--- views/cart.php
+++ views/cart.php
@@ -60,6 +60,7 @@ for ($i = 0; $i < count($carts); $i++) {
           <p>Thao tác</p>
         </div>
         <?php
+        $isDisabled = false;
         foreach ($carts as $index => $cart) {
           $last_item = $index == count($carts) - 1 ? "last-item" : "";
           $product = $cart->product;
@@ -67,6 +68,15 @@ for ($i = 0; $i < count($carts); $i++) {
           $thumb = $product->get_images($con)[0]->link;
           $name = $product->title;
           $quantity = $cart->qty;
+          $notify = "";
+          if ($product->quantity <= 0) {
+            $notify = "Thông báo: Đã hết hàng";
+            $isDisabled = true;
+          } else if ($quantity > $product->quantity) {
+            $notify = "Thông báo: Số sản phẩm còn lại không đủ (còn " . $product->quantity . " sản phẩm).";
+            $isDisabled = true;
+          }
+
           $price = $product->price;
           $price_discount = $product->get_price_discount();
           $item_total = $product->price * $quantity * (100 - $product->discount) / 100;
@@ -74,7 +84,7 @@ for ($i = 0; $i < count($carts); $i++) {
         ?>
           <div class="item <?= $last_item ?>">
             <div class="item_img">
-              <a href="product-detail?pro_id=<?=$product->id?>"><img src="images/<?= $thumb ?>" alt=""></a>
+              <a href="product-detail?pro_id=<?= $product->id ?>"><img src="images/<?= $thumb ?>" alt=""></a>
             </div>
             <p class="item_name"><?= $name ?></p>
             <div class="item-price">
@@ -82,31 +92,52 @@ for ($i = 0; $i < count($carts); $i++) {
               <span class="price-discount"><?= currency_format($price_discount) ?></span>
             </div>
             <div class="item_qty">
-              <a href="update-cart?cart_id=<?= $cart->id ?>&cart_qty=<?= $quantity - 1 ?>"><i class='bx bx-minus'></i></a>
-              <input type="text" oninput="handleInput(event)" onchange="handleChange(event, <?= $cart->id ?>)" value="<?= $quantity ?>" />
-              <a href="update-cart?cart_id=<?= $cart->id ?>&cart_qty=<?= $quantity + 1 ?>"><i class='bx bx-plus'></i></a>
+              <button>
+                <a href="update-cart?cart_id=<?= $cart->id ?>&cart_qty=<?= $quantity - 1 ?>"><i class='bx bx-minus'></i></a>
+              </button>
+              <input type="text" oninput="handleInput(event)" onchange="handleChange(event, <?= $cart->id ?>, <?= $product->quantity ?>, <?= $quantity ?>)" value="<?= $quantity ?>" />
+              <button onclick="handleIncreQty('<?= $cart->id ?>', '<?= $quantity + 1 ?>', '<?= $product->quantity ?>')">
+                <i class='bx bx-plus'></i>
+              </button>
             </div>
             <div class="item-total"><?= currency_format($item_total) ?></div>
             <a href="update-cart?cart_id=<?= $cart->id ?>&cart_qty=0"><i class='bx bx-x bx-rotate-90'></i> Xóa</a>
+            <p class="item-notify"><?= $notify ?></p>
           </div>
         <?php
         }
         ?>
         <div class="checkout">
-          <a href="checkout" class="btn-checkout"><?= currency_format($total) ?> <span></span>Thanh toán</a>
+          <button class="btn-checkout" <?= $isDisabled ? "disabled" : "" ?> onclick="window.open('checkout', '_self')">
+            <?= currency_format($total) ?> <span></span>Thanh toán
+          </button>
         </div>
     </div>
   </div>
-  <script>
+
+  <script type="text/javascript">
+    function handleIncreQty(cart_id, cart_qty, product_qty) {
+      if (cart_qty > product_qty) {
+        alert(`Sản phẩm còn lại không đủ(còn ${product_qty}).`);
+      } else {
+        window.open("update-cart?cart_id=<?= $cart->id ?>&cart_qty=<?= $quantity + 1 ?>", "_self");
+      }
+    }
+
     function handleInput(event) {
       let value = event.target.value;
       console.log(event.target.value);
       event.target.value = value.replace(/[^0-9]/g, '');
     }
 
-    function handleChange(event, cart_id) {
+    function handleChange(event, cart_id, product_qty, cart_qty) {
       let quantity = event.target.value;
-      window.open(`update-cart?cart_id=${cart_id}&cart_qty=${quantity}`, "_self")
+      if (quantity > product_qty) {
+        alert("Nhập số lượng vừa với số sản phẩm còn lại.");
+        event.target.value = cart_qty;
+      } else {
+        window.open(`update-cart?cart_id=${cart_id}&cart_qty=${quantity}`, "_self")
+      }
     }
   </script>
 <?php
