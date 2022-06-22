5b823ed6859d34a89b52649d2ab3009a437c9c7d
Nguyễn Đông <nguyenbadong01@gmail.com>
2022-06-23 00:09:38 +0700

    update logic and change quantity product in cart

--- controllers/cart/update-cart.php
+++ controllers/cart/update-cart.php
@@ -10,6 +10,11 @@ $cart_id = $_GET["cart_id"];
 $cart_qty = $_GET["cart_qty"];
 
 if ($cart_qty > 0) {
+  $cart = Cart::find_by_pk($con, $cart_id);
+  $cart->populated($con, "Product");
+  if ($cart_qty > $cart->product->quantity) {
+    return header("Location: cart");
+  }
   Cart::update_by_pk($con, $cart_id, array("cart_qty" => $cart_qty));
 } else if ($cart_qty == 0) {
   Cart::delete_by_pk($con, $cart_id);
